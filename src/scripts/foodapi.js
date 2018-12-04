c52ca0dc6b3671a3f690ff5b9e0b72157b55e963
Brittany Ramos-Janeway <bramosjaneway@gmail.com>
2018-12-04 16:09:15 -0600

    completed display foods project

--- src/scripts/foodapi.js
+++ src/scripts/foodapi.js
@@ -1,5 +1,37 @@
+
+const h1 = (title, style) => {
+    return `<h1 class="${style}">${title}</h1>`;
+};
+const h3 = (title, style) => {
+    return `<h3 class="${style}">${title}</h3>`;
+    };
+
+const div = (name, type, ethnicity, style) => {
+    return `<div class="${style}">
+            ${h1(name, "h1--style")}
+            ${h3(type, "h3--style")}
+            ${h3(ethnicity, "h3--style")}
+        </div>
+    `;
+    };
+
+let foodFactory = (name, type, ethnicity) => {
+    console.log(`${name}, ${type}, ${ethnicity},`);
+
+    let factoryResults = div(name, type, ethnicity, "div--style");
+        return `${factoryResults}`;
+    };
+
+let addFoodToSection = (item) => {
+   console.log(item);
+   document.querySelector("section").innerHTML += item;
+};
+
 fetch("http://localhost:8088/food")
     .then(foods => foods.json())
     .then(parsedFoods => {
-        console.table(parsedFoods);
+        parsedFoods.forEach(food => {
+            const foodAsHTML = foodFactory(food.name, food.type, food.ethnicity);
+            addFoodToSection(foodAsHTML);
+        });
     });
