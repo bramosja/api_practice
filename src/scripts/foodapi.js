9f95654c7523acbc5130a0d85623c7cc76a3b5f7
Brittany Ramos-Janeway <bramosjaneway@gmail.com>
2018-12-06 13:25:54 -0600

    completed (?) second practice with the Food Api

--- src/scripts/foodapi.js
+++ src/scripts/foodapi.js
@@ -1,37 +1,51 @@
+fetch("http://localhost:8088/food")
+    .then(foods => foods.json())
+    .then(parsedFoods => {
+        parsedFoods.forEach(food => {
+            fetch(`https://world.openfoodfacts.org/api/v0/product/${food.barcode}.json`)
+                .then(response => response.json())
+                .then(productInfo => {
+                    let productDetails = grabInfo(productInfo.product.ingredients_text, productInfo.product.countries, productInfo.product.serving_quantity, productInfo.product.nutriments.fat, productInfo.product.nutriments.sugars);
+            const foodAsHTML = foodFactory(food.name, food.type, food.ethnicity, productDetails);
+            addFoodToSection(foodAsHTML);
+                });
+        });
+    });
 
 const h1 = (title, style) => {
     return `<h1 class="${style}">${title}</h1>`;
 };
 const h3 = (title, style) => {
     return `<h3 class="${style}">${title}</h3>`;
-    };
+};
 
-const div = (name, type, ethnicity, style) => {
+const p = (ingredients, country, calories, fat, sugar) => {
+    return `<p>${ingredients}, ${country}, ${calories}, ${fat}, ${sugar}.</p>`;
+}
+
+
+const div = (name, type, ethnicity, infoString, style) => {
     return `<div class="${style}">
             ${h1(name, "h1--style")}
             ${h3(type, "h3--style")}
             ${h3(ethnicity, "h3--style")}
+            ${p(infoString)}
         </div>
     `;
-    };
+};
 
-let foodFactory = (name, type, ethnicity) => {
-    console.log(`${name}, ${type}, ${ethnicity},`);
-
-    let factoryResults = div(name, type, ethnicity, "div--style");
+let foodFactory = (name, type, ethnicity, infoString) => {
+    let factoryResults = div(name, type, ethnicity, infoString, "div--style");
         return `${factoryResults}`;
     };
 
 let addFoodToSection = (item) => {
-   console.log(item);
    document.querySelector("section").innerHTML += item;
 };
 
-fetch("http://localhost:8088/food")
-    .then(foods => foods.json())
-    .then(parsedFoods => {
-        parsedFoods.forEach(food => {
-            const foodAsHTML = foodFactory(food.name, food.type, food.ethnicity);
-            addFoodToSection(foodAsHTML);
-        });
-    });
+let grabInfo = (ingredients, country, calories, fat, sugar) => {
+    return `${ingredients}, ${country}, ${calories}, ${fat}, ${sugar}.`;
+};
+
+
+
